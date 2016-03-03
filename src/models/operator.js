d99a97de1e63bcc079380f933d8e93b732a07eda
David Raleigh <davidraleigh@gmail.com>
2016-03-03 13:41:32 -0800

    bugs with websocket handshake

--- src/models/operator.js
+++ src/models/operator.js
@@ -4,6 +4,39 @@
 import Model from 'ampersand-model'
 import MethodCollection from './method-collection'
 import has from 'amp-has'
+
+
+//function isNumeric(num) {
+//    if (num === 'true' || num === 'false' || num === false || num === true)
+//        return false;
+//    return !isNaN(num)
+//}
+//
+//function parseToArray(input, previousArray) {
+//    debugger;
+//    let result = previousArray ? previousArray : [];
+//    if (typeof input === 'string') {
+//        let splitInput = input.split(/[ ,]+/);
+//
+//        if (isNumeric(splitInput[0])) {
+//            for (var i = 0; i < splitInput.length; i++) {
+//                let number = splitInput[i];
+//                if (number.length > 0){
+//                    result.push(Number(number));
+//                    console.log("numerics push", result);
+//                }
+//            }
+//            return result;
+//        } else {
+//            console.log("not numeric", splitInput);
+//            return splitInput;
+//        }
+//    }
+//    result.push(input);
+//    return result;
+//}
+
+
 export default Model.extend({
     props: {
         operatorType: 'string'
@@ -50,8 +83,10 @@ export default Model.extend({
                     obj[param["parameterKey"]] = this.method.parseToWKTArray(param.parameterValue);
                 } else if (has(obj, param["parameterKey"])){
                     obj[param["parameterKey"]].push(param.parameterValue);
+                    //obj[param["parameterKey"]] = parseToArray(param.parameterValue, obj[param["parameterKey"]]);
                 } else {
                     obj[param["parameterKey"]] = [param.parameterValue];
+                    //obj[param["parameterKey"]] = parseToArray(param.parameterValue);
                 }
 
             }
